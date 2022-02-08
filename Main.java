0ccb312d70c975fef2b8b7418f309d6302f4bff5
moyama55 <moyama04@gmail.com>
2022-02-08 13:59:54 +0900

    first commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main{
+    public static void main(String[] args){
+        Y[] obj1 = new A[1];
+        Y[] obj2 = new B[1];
+
+        obj1[0] = new A(); 
+        obj[1] = new A();
+
+    }
+}
---
+++ Y.java
@@ -0,0 +1,4 @@
+public class Y implements X {
+    public abstract void a();
+    public abstract void b();
+}
