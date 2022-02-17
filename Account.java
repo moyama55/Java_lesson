fc614fe671b7a06c5f4aa23c33bb4a62ce581516
moyama55 <moyama04@gmail.com>
2022-02-17 20:22:13 +0900

    thiscommit

---
+++ Account.java
@@ -0,0 +1,8 @@
+public class Account{
+    int balance;
+    String accountNumber;
+    public String toString(){
+        return "?" + this.balance + "(" + "口座番号 :" + this.accountNumber + ")" ;
+    }
+    
+}
