7e6c059c5b6dfd11de187708cf2c7fe0e18a03eb
moyama55 <moyama04@gmail.com>
2022-03-10 18:40:51 +0900

    aaa

--- B.java
+++ B.java
@@ -2,4 +2,5 @@ public class B extends Y {
     public void a() {System.out.println("Ba")};
     public void b() {System.out.println("Bb")};
     public void c() {System.out.println("Bc")};
+    public void d() {System.out.println("bb")};
 }
